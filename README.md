d405a4ee065f6757a265cd3de8d3e2875daccee1
peta <peta@localhost.localdomain>
2015-06-19 13:55:34 +0900

    Add main files

---
+++ README.md
@@ -0,0 +1,37 @@
+# 計算誤差の検出
+
+以下の問題を解くプログラム。
+http://detail.chiebukuro.yahoo.co.jp/qa/question_detail/q11136503501
+リンク先ではなるべく実行が速く、なるべくコードが短いという点が
+評価軸になっているが、極めて個人的な趣味によりその辺りは投げ捨て
+読み易さを優先して本プログラムを作成した。
+
+問題(1):
+4個の一桁の数値に対する四則演算の結果が
+'分数の計算をちゃんとやれば'本当は10になるのに
+整数演算だと誤った結果になる例を全て出力。
+
+問題(2):
+問題(1)の浮動小数点数版。
+
+## 使い方:
+`$ python calc_error_detection.py int`
+`$ python calc_error_detection.py float`
+
+## 出力:
+式 = 真の答え (誤差を含んだ答え)
+式 = 真の答え (誤差を含んだ答え)
+式 = 真の答え (誤差を含んだ答え)
+...
+
+## 注意点:
+枝刈りとかしてないので激遅です。
+NaNを表現するためにNoneを使用しています。
+
+## 動作確認環境:
+`$ cat /etc/redhat-release
+Fedora release 22 (Twenty Two)
+$ uname -rvo
+4.0.4-303.fc22.x86_64 #1 SMP Thu May 28 12:37:06 UTC 2015 GNU/Linux
+$ python -V
+Python 2.7.10`
---
+++ calc-error-detection.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+計算誤差の検出
+
+使い方:
+python calc_error_detection.py int
+python calc_error_detection.py float
+"""
+
+# 以下の行を有効にすると標準ライブラリの分数クラスを使用する(高機能な分遅い)
+# from fractions import Fraction
+import sys
+
+__author__ = "peta_okechan"
+__status__ = "development"
+__version__ = "0.0.1"
+__date__ = "2015.06.19"
+
+
+# 演算子
+operators = [
+    ('+', lambda a, b: a + b),
+    ('-', lambda a, b: a - b),
+    ('*', lambda a, b: a * b),
+    ('/', lambda a, b: a / b),
+]
+
+# 利用する数字のリスト
+digits = range(0, 10)
+
+# 目指す真の答え
+answer = 10
+
+# 式の項数
+terms = 4
+
+
+if 'Fraction' not in locals():
+    class Fraction():
+        '''
+        分数クラス（約分非対応）
+        実はPython 2.6から分数モジュール（fractions）が追加されているので
+        必ずしも実装する必要はないのだが、
+        四則演算用の特殊メソッドの例を示すため敢えて実装している。
+        '''
+        def __init__(self, num=0, denom=1):
+            self.num = num
+            self.denom = denom
+
+        def __add__(self, other):
+            if self.denom != other.denom:
+                return self * Fraction(other.denom, other.denom)\
+                    + other * Fraction(self.denom, self.denom)
+            return Fraction(self.num + other.num, self.denom)
+
+        def __sub__(self, other):
+            return self + -other
+
+        def __mul__(self, other):
+            return Fraction(self.num * other.num, self.denom * other.denom)
+
+        def __div__(self, other):
+            return self * Fraction(other.denom, other.num)
+
+        def __eq__(self, other):
+            if other is None:
+                return False
+            return self.num * other.denom == other.num * self.denom
+
+        def __neg__(self):
+            return Fraction(-self.num, self.denom)
+
+
+class Constant():
+    '''
+    定数式
+    '''
+    def __init__(self, num):
+        self.num = num
+
+    def eval(self, evaltype):
+        '''
+        定数を評価する
+        evaltype: 評価時の型
+        '''
+        return evaltype(self.num)
+
+    def __repr__(self):
+        return self.num.__repr__()
+
+
+class Binomial():
+    '''
+    二項式
+    ツリーで式を表現する。
+    計算結果がNaNになる場合はNoneを返す。
+    '''
+    def __init__(self, lhs, op, rhs):
+        self.lhs = lhs
+        self.op = op
+        self.rhs = rhs
+
+    def eval(self, evaltype):
+        '''
+        式を評価する
+        evaltype: 評価時の型
+        '''
+        lv = self.lhs.eval(evaltype)
+        rv = self.rhs.eval(evaltype)
+        if self.op[0] == '/' and rv == evaltype(0):
+            return None
+        if lv is None or rv is None:
+            return None
+        return self.op[1](lv, rv)
+
+    def __repr__(self):
+        return '({} {} {})'.format(
+            self.lhs.__repr__(),
+            self.op[0],
+            self.rhs.__repr__()
+        )
+
+
+class NumbersGenerator():
+    '''
+    総当たりで数字のリストを生成するクラス
+    数字を生成するのではなく、数字のリストを生成する点に注意。
+    '''
+    def __init__(self, length):
+        self.numbers = [None for i in range(length)]
+        self.length = length
+
+    def __iter__(self, depth=0):
+        if self.length > depth:
+            for i in digits:
+                self.numbers[depth] = i
+                for n in self.__iter__(depth + 1):
+                    yield self.numbers
+        else:
+            yield self.numbers
+
+
+class ExpressionGenerator():
+    '''
+    総当たりで式を生成するクラス
+    '''
+    def __init__(self, numbers):
+        self.numbers = numbers
+
+    def __iter__(self, left=0, right=None):
+        if right is None:
+            right = len(self.numbers) - 1
+
+        if left == right:
+            yield Constant(self.numbers[left])
+        else:
+            for i in range(left, right):
+                for e1 in self.__iter__(left, i):
+                    for e2 in self.__iter__(i + 1, right):
+                        for op in operators:
+                            yield Binomial(e1, op, e2)
+
+
+def check(length, evaltype):
+    '''
+    分数で評価するとanswerになるが、evaltypeで評価すると
+    誤差でanswerにならない式を表示する。
+    '''
+    numgen = NumbersGenerator(length)
+    for numbers in numgen:
+        expgen = ExpressionGenerator(numbers)
+        for exp in expgen:
+            val = exp.eval(evaltype)
+            fracval = exp.eval(Fraction)
+            if evaltype(answer) != val and Fraction(answer) == fracval:
+                print('{} = {} ({})'.format(exp, answer, repr(val)))
+
+if __name__ == '__main__':
+    types = {
+        'int': int,
+        'float': float,
+    }
+    if len(sys.argv) == 2 and sys.argv[1] in types:
+        check(terms, types[sys.argv[1]])
